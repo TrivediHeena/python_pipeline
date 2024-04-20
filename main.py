ff4aff561125405c8ebc13148b0856d66ec73f64
trivediheena <hntrivedi72@gmail.com>
2024-04-20 22:12:45 +0530

    Small Changes

--- main.py
+++ main.py
@@ -3,25 +3,28 @@
 # Press Shift+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 import psutil
-from flask import Flask, request,render_template
+from flask import Flask, request, render_template
 
-flask=Flask(__name__)
+flask = Flask(__name__)
+
 
 @flask.route('/')
 def watch():
-    cpu_monitor=psutil.cpu_percent()
-    memory_monitor=psutil.virtual_memory().percent
-    message=None
-    if cpu_monitor>80 or memory_monitor>80:
-        message="High CPU or Memmory Utilized"
+    cpu_monitor = psutil.cpu_percent()
+    memory_monitor = psutil.virtual_memory().percent
+    message = None
+    if cpu_monitor > 80 or memory_monitor > 80:
+        message = "High CPU or Memmory Utilized"
     return f'CPU Utilization {cpu_monitor} and Memory Utilization {memory_monitor}'
-    #return render_template('index.html', cpu_monitor=cpu_monitor,memory_monitor=memory_monitor,message=message)
+    # return render_template('index.html', cpu_monitor=cpu_monitor,memory_monitor=memory_monitor,message=message)
+
 
 def main():
-    flask.run(debug=True,host='0.0.0.0')
+    flask.run(debug=True, host='0.0.0.0')
+
+
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     main()
-
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
